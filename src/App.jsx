f8762457ad116a22cdea003e889be61d174ced12
Anvarbek <anvarbektoshmamatov888@gmail.com>
2024-01-05 15:40:35 +0500

    A

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import "../src/App.css"
+function App() {
+
+
+  return (
+    <div>
+       
+       <p className="FISH">Anvarbek Toshmamatov</p>
+
+       <div id="Telegram">
+         <h2>Telegram.</h2>
+         <h3>Link: <a href="https://t.me/Alhamdulillah_bmats">Anvarbek</a></h3>
+       </div>
+
+       <div id="Telegram_Kanal">
+          <h2>Telegram Kanal.</h2>
+          <h3>Link: <a href="t.me/frontend_bmats">KanalLink</a></h3>
+       </div>
+
+       <div id="Linkden">
+          <h2>Linkden.</h2>
+          <h3>Link: <a href="https://www.linkedin.com/in/anvarbek-toshmamatov-30963b297/?locale=en_US">AnvarbekLink</a></h3>
+       </div>
+
+       <div id="Linkden">
+          <h2>Git Hub</h2>
+          <h3>Link: <a href="https://github.com/Anvarbek07088">GitHubLink</a></h3>
+       </div>
+    </div>
+  )
+}
+
+export default App
